11305574a869e1c14bee697cd78de3456a05a43d
Ilia Mitrokhin <i.mitrokhin@innopolis.university>
2025-04-15 20:39:07 +0300

    initialize cassandra

--- app/app.sh
+++ app/app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start ssh server
-service ssh restart 
+service ssh restart
 
 # Starting the services
 bash start-services.sh
@@ -14,13 +14,57 @@ pip install -r requirements.txt
 
 # Package the virtual env.
 venv-pack -o .venv.tar.gz
+echo "Initializing cassandra"
+until cqlsh cassandra-server -e "DESCRIBE KEYSPACES" > /dev/null 2>&1; do
+  sleep 3
+done
+echo "Cassandra initialized"
 
+cat > /tmp/initialize_cassandra_keyspace.cql << EOF
+CREATE KEYSPACE IF NOT EXISTS big_data_assignment
+WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
+
+
+USE big_data_assignment;
+
+CREATE TABLE IF NOT EXISTS vocabulary (
+    term text PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS document_main_content (
+    doc_id text PRIMARY KEY,
+    content text
+);
+
+CREATE TABLE IF NOT EXISTS doc_freq (
+    term text PRIMARY KEY,
+    doc_freq int
+);
+
+CREATE TABLE IF NOT EXISTS doc_len (
+    doc_id text PRIMARY KEY,
+    doc_len int
+);
+
+CREATE TABLE IF NOT EXISTS term_freq (
+    term text,
+    doc_id text,
+    term_freq int,
+    PRIMARY KEY (term, doc_id)
+);
+EOF
+
+# Initialize cassandra
+cqlsh cassandra-server -f /tmp/initialize_cassandra_keyspace.cql
+
+
+echo "Cassandra tables created succesfully"
 # Collect data
 bash prepare_data.sh
 
 
 # Run the indexer
-bash index.sh data/sample.txt
+bash index.sh 
 
 # Run the ranker
-bash search.sh "this is a query!"
+bash search.sh "How are you?"
